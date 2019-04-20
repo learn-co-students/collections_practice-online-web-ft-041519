2f804f72f2d1eabea909b2db6e862ed825bde74a
Annia Forney <anniaforney@gmail.com>
2019-04-20 23:31:07 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,44 @@
+def sort_array_asc(array)
+  sort_array_asc index = [1, 2, 3, 4, 5]
+  return sort_array_asc
+end 
 
+def sort_array_desc(array)
+  sort_array_desc index = [5, 4, 3, 2, 1,]
+  array.sort
+  return sort_array_desc
+end 
+
+def sort_array_char_count(string)
+  char string 
+  string[1]
+  string[2]
+  string[3]
+  string[4]
+  string[5]
+  sort_array_char_count.sort 
+  return sort_array_char_count
+end 
+
+def swap_elements(array)
+  elements = [a, b, c, d, e]
+  swap_elements_from_to(["a", "b", "c"],1,2,3)
+  
+  swap_elements_from_to (["a", "b", "c"],2,4,3)
+  return swap_elements
+end 
+
+def reverse_array 
+end 
+
+def kesha_maker
+end 
+
+def find_a
+end 
+
+def sum_array
+end 
+
+def add_s
+end 
